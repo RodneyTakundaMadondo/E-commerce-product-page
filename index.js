364a1d55ee880195ea46331bf420bd6b8925cb34
Rodney Madondo <madondotakunda09@gmail.com>
2024-08-08 14:41:43 +0200

    completed the javascript functionality of the app

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let plusBtn = document.getElementById("add")
 let subtract = document.getElementById("subtract")
 let addCart = document.getElementById("add-cart");
 let checkoutBtn = document.getElementById("checkout");
-
+let nextBtn = document.querySelector(".prod")
 
 let navlist = document.querySelector(".nav-list");
 let cartContent = document.querySelector(".cart-content");
@@ -14,6 +14,7 @@ let productName = document.getElementById("product-name");
 let  price = document.getElementById("price");
 let cartItem = document.querySelector(".cart-content__item");
 let cartStatus = document.getElementById("cart-status")
+let cartAmount = document.querySelector(".orange");
 
 
 hamburgerBtn.addEventListener("click",()=>{
@@ -37,12 +38,13 @@ subtract.addEventListener('click',()=>{
    }
 })
 
-console.log(checkoutBtn)
 $(addCart).click(()=>{
-    
+    let cartQuantity = quantity.textContent;
     if(quantity.textContent >0){
         checkoutBtn.classList.remove("hidden")
         cartStatus.classList.add("hidden")
+        cartAmount.classList.remove("hidden");
+        cartAmount.textContent = cartQuantity;
         let itemInCart =`
     <div class="product">
             <img src="./images/image-product-1-thumbnail.jpg" alt="">
@@ -53,11 +55,12 @@ $(addCart).click(()=>{
               <span class="cart-price">
                 ${price.textContent}
               </span>
+              
               <span class="cart-quantity">
-                x${quantity.textContent}
+               x<span class="inner-quantity"> ${cartQuantity}</span>
               </span>
               <span class="cart-total">
-                $${Number(price.textContent) * Number(quantity.textContent)}
+                $${Number(price.textContent) * Number(cartQuantity)}
               </span>
             </p>
           </div>
@@ -66,13 +69,26 @@ $(addCart).click(()=>{
           </button>
 
     `
-
     cartItem.innerHTML = itemInCart
-    }else{
+    let innerQuantity = document.querySelector(".inner-quantity")
+    let deleteBtn = document.querySelector(".delete");
+    let cartTotal = document.querySelector(".cart-total");
+
+    $(deleteBtn).click(()=>{
+      if(cartAmount.textContent>0 && innerQuantity.textContent >0){
+        cartAmount.textContent --;
+        innerQuantity.textContent --;
+        cartTotal.textContent = `$${Number(innerQuantity.textContent) *Number(price.textContent)}`;
+      }
+    })
+    }
+    
+    else{
         return
     }
     
 })
+
 /*
 when we click the add button we wanna fetch the h1 ,
 the price of the item ,the quantity and calculate the tot price 
