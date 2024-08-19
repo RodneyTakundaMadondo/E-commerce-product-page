53846b291b244bd1a8c6381c25d2f79f7c3e073a
Rodney Madondo <madondotakunda09@gmail.com>
2024-08-19 16:26:04 +0200

    completed added js for popup button functionality

--- index.js
+++ index.js
@@ -12,114 +12,140 @@ let navlist = document.querySelector(".nav-list");
 let cartContent = document.querySelector(".cart-content");
 let quantity = document.getElementById("quantity");
 let productName = document.getElementById("product-name");
-let  price = document.getElementById("price");
+let price = document.getElementById("price");
 let cartItem = document.querySelector(".cart-content__item");
 let cartStatus = document.getElementById("cart-status")
 let cartAmount = document.querySelector(".orange");
 let focusImg = document.querySelector(".alt-img-focus");
-let  productImgContainer = document.querySelector(".product-image")
+let productImgContainer = document.querySelector(".product-image")
 let altImgs = productImgContainer.querySelectorAll(".alt-img");
 let imgInFocus = document.querySelector(".img-in-focus");//this the image we see after clicking one of the tiny images
 let focusAltimgs = focusImg.querySelectorAll(".alt-img");
 let prev = focusImg.querySelector(".previous");
 let next = focusImg.querySelector(".next");
 
-altImgs.forEach((altImg)=>{
-  altImg.addEventListener("click",()=>{
-    let img = altImg.querySelector("#alt-view").src.replace("-thumbnail","");//looking for this specific id in this container and then getting the src, then remove the part of that src that we dont want
-    imgInFocus.innerHTML = `
-        <img id="pop-img" src="${img}" alt="">
-    `
+
+altImgs.forEach((altImg) => {
+  altImg.addEventListener("click", () => {
+    let img = altImg.querySelector("#alt-view").src.replace("-thumbnail", "");//looking for this specific id in this container and then getting the src, then remove the part of that src that we dont want
+
+    $("#popup").attr("src", `${img}`)
     $(focusImg).toggleClass("hidden");
-   $(".overlay").toggleClass("hidden");
+    $(".overlay").toggleClass("hidden");
   })
 })
-focusAltimgs.forEach((focusAltimg)=>{
-      focusAltimg.addEventListener("click", ()=>{
-        let img = focusAltimg.querySelector("#alt-view").src.replace("-thumbnail","");
-        imgInFocus.innerHTML = `
-        <img id="pop-img" src="${img}" alt="">
-    `
-      })
+focusAltimgs.forEach((focusAltimg) => {
+  focusAltimg.addEventListener("click", () => {
+    let img = focusAltimg.querySelector("#alt-view").src.replace("-thumbnail", "");
+    $("#popup").attr("src", `${img}`)
+  })
 })
 
 
-$(".close__btn").click(()=>{
+$(".close__btn").click(() => {
   $(".overlay").addClass("hidden");
   $(focusImg).addClass("hidden")
 })
 
 
-$(hamburgerBtn).click(()=>{
+$(hamburgerBtn).click(() => {
   navlist.classList.remove("hidden");
 })
- function updateNavList(){
-  if(window.innerWidth>=768){
+function updateNavList() {
+  if (window.innerWidth >= 768) {
     navlist.classList.remove("hidden")
-  }else{
+  } else {
     navlist.classList.add("hidden")
   }
- }
-  updateNavList();
-   window.addEventListener("resize",updateNavList)
-    
-$(closeBtn).click(()=>{
+}
+updateNavList();
+window.addEventListener("resize", updateNavList)
+
+$(closeBtn).click(() => {
   navlist.classList.add("hidden")
 })
 
-cartBtn.addEventListener('click',()=>{
-    cartContent.classList.toggle("hidden");
+cartBtn.addEventListener('click', () => {
+  cartContent.classList.toggle("hidden");
 })
 
-plusBtn.addEventListener('click',()=>{
-    quantity.textContent++;
+plusBtn.addEventListener('click', () => {
+  quantity.textContent++;
 })
 
-subtract.addEventListener('click',()=>{
-// console.log(typeof Number(quantity.textContent));
-   if(Number(quantity.textContent) === 0){
+subtract.addEventListener('click', () => {
+  if (Number(quantity.textContent) === 0) {
     return
-   }else{
-    quantity.textContent --;
-   }
+  } else {
+    quantity.textContent--;
+  }
 })
 let img = document.querySelector("#main-item");
 let imgNum = 1;
-$(".product-image__previous").click(()=>{
+$(".product-image__previous").click(() => {
   /*we can get the image and manipulate the src of it
   when it is less than 1 go back to 4 and loop that way */
   imgNum--;
-  if(imgNum<=0){
+  if (imgNum <= 0) {
     imgNum = 4;
   }
   manipulateImg(imgNum)
 })
-$(".product-image__next").click(()=>{
+$(".product-image__next").click(() => {
   imgNum++;
-  if(imgNum>4){
+  if (imgNum > 4) {
     imgNum = 1;
   }
   manipulateImg(imgNum);
-  
+
 })
 
-function manipulateImg(number){
+function manipulateImg(number) {
   let imgsrc = `./images/image-product-${number}.jpg`;
   img.src = imgsrc;
 }
-// let imgfocus = document.querySelector("#pop-img")
-// $(prev).click(()=>{
-//   console.log(imgfocus);
-// })
+let imgpop = $('#popup');
+$(prev).click(() => {
 
-$(addCart).click(()=>{
-    let cartQuantity = quantity.textContent;
-    if(quantity.textContent >0){
-        checkoutBtn.classList.remove("hidden")
-        cartStatus.classList.add("hidden")
-        cartAmount.classList.remove("hidden");
-        cartAmount.textContent = cartQuantity;
-        let itemInCart =`
+  let initsrc = imgpop.attr("src");
+
+  /*i get the index of the number and i take that value and 
+  keeping that value in mind i update the whole string to have what 
+  i want specifically the number part */
+  let dot = initsrc.lastIndexOf(".");
+  let dash = initsrc.lastIndexOf("-");
+  let imgNum = initsrc.slice(dash + 1, dot);
+
+  imgNum--;
+  if (imgNum <= 0) {
+    imgNum = 4;
+  }
+  imgpop.attr("src", `http://127.0.0.1:5500/images/image-product-${imgNum}.jpg`);
+})
+
+$(next).click(() => {
+
+  let initsrc = imgpop.attr("src");
+  let dot = initsrc.lastIndexOf(".");
+  let dash = initsrc.lastIndexOf("-");
+  let imgNum = initsrc.slice(dash + 1, dot);
+
+  imgNum++;
+  if (imgNum > 4) {
+    imgNum = 1;
+  }
+  imgpop.attr("src", `./images/image-product-${imgNum}.jpg`);
+})
+
+
+$(addCart).click(() => {
+  let cartQuantity = quantity.textContent;
+  if (quantity.textContent > 0) {
+    checkoutBtn.classList.remove("hidden")
+    cartStatus.classList.add("hidden")
+    cartAmount.classList.remove("hidden");
+    cartAmount.textContent = cartQuantity;
+    let itemInCart = `
     <div class="product">
             <img src="./images/image-product-1-thumbnail.jpg" alt="">
           </div>
@@ -148,22 +174,18 @@ $(addCart).click(()=>{
     let deleteBtn = document.querySelector(".delete");
     let cartTotal = document.querySelector(".cart-total");
 
-    $(deleteBtn).click(()=>{
-      if(cartAmount.textContent>0 && innerQuantity.textContent >0){
-        cartAmount.textContent --;
-        innerQuantity.textContent --;
-        cartTotal.textContent = `$${Number(innerQuantity.textContent) *Number(price.textContent)}`;
+    $(deleteBtn).click(() => {
+      if (cartAmount.textContent > 0 && innerQuantity.textContent > 0) {
+        cartAmount.textContent--;
+        innerQuantity.textContent--;
+        cartTotal.textContent = `$${Number(innerQuantity.textContent) * Number(price.textContent)}`;
       }
     })
-    }
-    
-    else{
-        return
-    }
-    
+  }
+
+  else {
+    return
+  }
+
 })
 
-/*
-when we click the add button we wanna fetch the h1 ,
-the price of the item ,the quantity and calculate the tot price 
-*/
